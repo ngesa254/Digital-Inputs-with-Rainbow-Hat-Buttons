a0dd821f10514e8e9b44828727ed738945e4db22
ngesa marvin <marvinngesa@gmail.com>
2018-01-20 03:38:23 +0300

    create an instance of PeripheralManagerService and the Button

--- app/src/main/java/com/droidmarvin/digitalinputswithrainbowhatbuttons/MainActivity.java
+++ app/src/main/java/com/droidmarvin/digitalinputswithrainbowhatbuttons/MainActivity.java
@@ -3,10 +3,21 @@ package com.droidmarvin.digitalinputswithrainbowhatbuttons;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.things.pio.Gpio;
+import com.google.android.things.pio.PeripheralManagerService;
+
 public class MainActivity extends Activity {
+
+    private static final String BUTTON_PIN = "BCM21";
+
+    private Gpio mButtonGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        PeripheralManagerService service = new PeripheralManagerService();
+
+
     }
 }
